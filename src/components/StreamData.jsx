de7a73dac180d3bbe2fce2768a65dd368870338b
safouene <safouene-abbachi@hotmail.fr>
2022-03-17 19:50:11 +0100

    latest implementation

---
+++ src/components/StreamData.jsx
@@ -0,0 +1,102 @@
+import React from 'react';
+import { Line, Bar } from 'react-chartjs-2';
+import 'chartjs-adapter-luxon';
+import StreamingPlugin from 'chartjs-plugin-streaming';
+import DataLabelsPlugin from 'chartjs-plugin-datalabels';
+
+import Chart from 'chart.js/auto';
+import { useDataStreamContextContext } from '../context/AppContext';
+
+Chart.register(StreamingPlugin, DataLabelsPlugin);
+
+const StreamData = () => {
+  const { state } = useDataStreamContextContext();
+  console.log(state);
+  const onRefresh = (stream) => {
+    stream.datasets.forEach((dataset) => {
+      dataset.data.push({
+        x: Date.now(),
+        y:
+          stream.style === 'line'
+            ? (Math.random() * 10).toFixed()
+            : (Math.random() * 1).toFixed(),
+      });
+    });
+  };
+
+  return (
+    <div
+      style={
+        {
+          // width: '45%',
+          // height: '35%',
+        }
+      }
+    >
+      {state.map((datasets) => {
+        let data = datasets;
+        let options = {
+          scales: {
+            xAxes: {
+              type: 'realtime',
+              realtime: {
+                onRefresh: () => onRefresh(datasets),
+                duration: 20000,
+                delay: 1000,
+                refresh: 1000,
+              },
+            },
+
+            yAxes: {
+              title: {
+                display: true,
+                text: 'Value',
+              },
+              scaleLabel: {
+                display: true,
+                fontFamily: 'Arial',
+                labelString: 'Moment',
+                fontSize: 20,
+                fontColor: '#6c757d',
+              },
+              ticks: {
+                max: 20,
+                min: 0,
+              },
+            },
+          },
+          plugins: {
+            datalabels: {
+              backgroundColor: (context) => context.dataset.borderColor,
+              padding: 4,
+              borderRadius: 4,
+              clip: true,
+              color: 'white',
+              font: {
+                weight: 'bold',
+              },
+              formatter: (value) => value.y,
+            },
+          },
+        };
+        if (datasets.style === 'line') {
+          return (
+            <div key={datasets.title}>
+              <h3>Click on color boxes to choose a stream</h3>
+              <Line data={data} options={options} />
+            </div>
+          );
+        } else {
+          return (
+            <div key={datasets.title}>
+              <h3>Click on color boxes to choose a stream</h3>
+              <Bar data={data} options={options} />
+            </div>
+          );
+        }
+      })}
+    </div>
+  );
+};
+
+export default StreamData;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import AppContext from './context/AppContext';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AppContext>
+      <App />
+    </AppContext>
   </React.StrictMode>,
   document.getElementById('root')
 );
